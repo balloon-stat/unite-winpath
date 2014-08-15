c09963f9c9279522dbbcfac8dcd5811b7a57d90c
balioon-stat <hsho899@gmail.com>
2014-08-15 12:38:54 +0900

    modify run powershell with option ExecutionPolicy

--- autoload/unite/kinds/modify_path.bat
+++ autoload/unite/kinds/modify_path.bat
@@ -1,7 +1,5 @@
 @echo off
 cd /d %~dp0
-powershell Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
-if %1 equ add    powershell -File .\add_path.ps1    %2
-if %1 equ delete powershell -File .\delete_path.ps1 %2
-powershell Set-ExecutionPolicy -Scope CurrentUser Restricted
+if %1 equ add    powershell -ExecutionPolicy RemoteSigned -File .\add_path.ps1    %2
+if %1 equ delete powershell -ExecutionPolicy RemoteSigned -File .\delete_path.ps1 %2
 
